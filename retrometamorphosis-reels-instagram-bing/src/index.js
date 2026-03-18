b63380df0503474f3af937fe62cfdb71d5941d85
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-03-18 04:28:22 -0300

    Implement main function for Instagram Bing integration

---
+++ retrometamorphosis-reels-instagram-bing/src/index.js
@@ -0,0 +1,8 @@
+const integration = require('./integration');
+
+function main() {
+  console.log("Starting Retrometamorphosis Reels Instagram Bing integration...");
+  integration.run();
+}
+
+main();
